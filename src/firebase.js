f1a6f61c4a0635889c32fce9550ebd1474a1cd0c
dripminister <95714820+dripminister@users.noreply.github.com>
2023-09-05 21:50:03 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,7 @@ import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-	apiKey: "AIzaSyAE-f22C8Izg23irEPoYZ6knAaUM7-tAWU",
-	authDomain: "zoom-356f1.firebaseapp.com",
-	projectId: "zoom-356f1",
-	storageBucket: "zoom-356f1.appspot.com",
-	messagingSenderId: "347369471836",
-	appId: "1:347369471836:web:27873acc1f1ffacecccb99",
+//Firebase config
 }
 
 const app = initializeApp(firebaseConfig)
